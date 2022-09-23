6fc98646d313c479977d243529be53c0d15a3c04
rjquiroz02 <rjquiroz02@unitec.edu>
2022-09-23 16:25:18 -0600

    Commit11

--- src/examen2p2_ricardoquiroz/AdminCarro.java
+++ src/examen2p2_ricardoquiroz/AdminCarro.java
@@ -5,10 +5,94 @@
  */
 package examen2p2_ricardoquiroz;
 
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
 /**
  *
  * @author rjqer
  */
 public class AdminCarro {
+    private ArrayList<Carro> listacarros = new ArrayList();
+    private File archivo = null;
+
+    public AdminCarro(String path) {
+        archivo = new File(path);
+    }
+
+    public ArrayList<Carro> getListacarros() {
+        return listacarros;
+    }
+
+    public void setListacarros(ArrayList<Carro> listacarros) {
+        this.listacarros = listacarros;
+    }
+
+    public File getArchivo() {
+        return archivo;
+    }
+
+    public void setArchivo(File archivo) {
+        this.archivo = archivo;
+    }
+
+    @Override
+    public String toString() {
+        return "listacarros" + listacarros;
+    }
+
+    
+    public void setCarro(Carro p) {
+        this.listacarros.add(p);
+    }
+
+    public void cargarArchivo() {
+        try {            
+            listacarros = new ArrayList();
+            Carro temp;
+            if (archivo.exists()) {
+                  FileInputStream entrada
+                    = new FileInputStream(archivo);
+                ObjectInputStream objeto = new ObjectInputStream(entrada);
+                try {
+                    while ((temp = (Carro) objeto.readObject()) != null) {
+                        listacarros.add(temp);
+                    }
+                } catch (EOFException e) {
+                    //encontro el final del archivo
+                }
+                objeto.close();
+                entrada.close();
+            } //fin if           
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void escribirArchivo() {
+        FileOutputStream fw = null;
+        ObjectOutputStream bw = null;
+        try {
+            fw = new FileOutputStream(archivo);
+            bw = new ObjectOutputStream(fw);
+            for (Carro t : listacarros) {
+                bw.writeObject(t);
+            }
+            bw.flush();
+        } catch (Exception ex) {
+        } finally {
+            try {
+                bw.close();
+                fw.close();
+            } catch (Exception ex) {
+            }
+        }
+    }
+    
     
 }
