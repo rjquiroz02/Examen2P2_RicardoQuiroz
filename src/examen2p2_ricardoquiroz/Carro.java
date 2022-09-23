c98e51c42b4dea38e71158bbb415d23e5764a7f4
rjquiroz02 <rjquiroz02@unitec.edu>
2022-09-23 14:58:51 -0600

    Commit5

---
+++ src/examen2p2_ricardoquiroz/Carro.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package examen2p2_ricardoquiroz;
+
+/**
+ *
+ * @author rjqer
+ */
+public class Carro {
+    String marca, modelo;
+    int anofab;
+    String estado;
+    int costorep;
+
+    public Carro() {
+    }
+
+    public Carro(String marca, String modelo, int anofab, String estado, int costorep) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.anofab = anofab;
+        this.estado = estado;
+        this.costorep = costorep;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getAnofab() {
+        return anofab;
+    }
+
+    public void setAnofab(int anofab) {
+        this.anofab = anofab;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public int getCostorep() {
+        return costorep;
+    }
+
+    public void setCostorep(int costorep) {
+        this.costorep = costorep;
+    }
+
+    @Override
+    public String toString() {
+        return marca + ", modelo=" + modelo + ", estado=" + estado + ", costorep=" + costorep;
+    }
+    
+    
+    
+}
---
+++ src/examen2p2_ricardoquiroz/Empleado.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package examen2p2_ricardoquiroz;
+
+/**
+ *
+ * @author rjqer
+ */
+public class Empleado {
+    private String nombre;
+    private int edad;
+    private String id;
+    private int creparados;
+
+    public Empleado() {
+    }
+
+    public Empleado(String nombre, int edad, String id, int creparados) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.id = id;
+        this.creparados = creparados;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getCreparados() {
+        return creparados;
+    }
+
+    public void setCreparados(int creparados) {
+        this.creparados = creparados;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + ", id=" + id;
+    }
+    
+    
+    
+    
+}
